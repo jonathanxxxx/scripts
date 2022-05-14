b49dacb2a081aa3c8106e8c49981d0cbec7a2667
jonathanxxxx <105544392+jonathanxxxx@users.noreply.github.com>
2022-05-14 13:03:50 +0100

    Update Zoom.js

--- Zoom.js
+++ Zoom.js
@@ -2,7 +2,7 @@ function zoomOutMobile() {
   var viewport = document.querySelector('meta[name="viewport"]');
 
   if ( viewport ) {
-    viewport.content = "initial-scale=0.1";
+    //viewport.content = "initial-scale=0.1";
     viewport.content = "width=1200";
   }
 }
