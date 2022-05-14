bad03497bb0e880c315b894b577c3c684f64beb8
jonathanxxxx <105544392+jonathanxxxx@users.noreply.github.com>
2022-05-14 11:33:10 +0100

    Create Zoom.js

---
+++ Zoom.js
@@ -0,0 +1,7 @@
+function zoom(scale) {
+    document.body.style.transform = "scale(" + scale + ")";
+    document.body.style.transformOrigin = "top left";
+    document.body.style.width = (100 / scale) + "%";
+    document.body.style.height = (100 / scale) + "%";
+};
+zoom(1.25);
